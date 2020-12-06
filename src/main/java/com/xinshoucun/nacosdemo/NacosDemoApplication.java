14cf5eda5d11f68579bcfac6cfb05ced4fb03627
szgd <a615893265@163.com>
2020-12-06 10:07:33 +0800

    add nacos conf

--- src/main/java/com/xinshoucun/nacosdemo/NacosDemoApplication.java
+++ src/main/java/com/xinshoucun/nacosdemo/NacosDemoApplication.java
@@ -1,9 +1,14 @@
 package com.xinshoucun.nacosdemo;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySources;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySources(value = {
+        @NacosPropertySource(dataId = "application.properties", autoRefreshed = true)
+})
 public class NacosDemoApplication {
 
     public static void main(String[] args) {
